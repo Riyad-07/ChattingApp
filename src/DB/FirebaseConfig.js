c9c5251e8e3845daeae145103dea60098e852f90
Riyad-07 <samimsamim1998@gmail.com>
2024-10-18 00:04:11 +0600

    db update

--- src/DB/FirebaseConfig.js
+++ src/DB/FirebaseConfig.js
@@ -1,20 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 const firebaseConfig = {
-  apiKey: "AIzaSyB_kDXIeSfPaVPsY22bj9tvSRJrQAL-tvk",
-  authDomain: "chatapp-c9195.firebaseapp.com",
-  databaseURL: "https://chatapp-c9195-default-rtdb.firebaseio.com",
-  projectId: "chatapp-c9195",
-  storageBucket: "chatapp-c9195.appspot.com",
-  messagingSenderId: "257202542770",
-  appId: "1:257202542770:web:3f3ab19e99cdb9370dd1ab"
+    
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app)
 
 export default firebaseConfig
