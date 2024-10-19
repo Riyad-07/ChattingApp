8bcf953a7e8fdbc93eb456c241ac74e017ff4f8b
Riyad-07 <samimsamim1998@gmail.com>
2024-10-19 13:47:38 +0600

    add env file

--- src/DB/FirebaseConfig.js
+++ src/DB/FirebaseConfig.js
@@ -17,21 +17,19 @@
 // export default firebaseConfig
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
+
+import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyB_kDXIeSfPaVPsY22bj9tvSRJrQAL-tvk",
-  authDomain: "chatapp-c9195.firebaseapp.com",
-  databaseURL: "https://chatapp-c9195-default-rtdb.firebaseio.com",
-  projectId: "chatapp-c9195",
-  storageBucket: "chatapp-c9195.appspot.com",
-  messagingSenderId: "257202542770",
-  appId: "1:257202542770:web:3f3ab19e99cdb9370dd1ab"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  databaseURL: import.meta.env.VITE_databaseURL,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
