9edcfad8362118db66990dabee32047e8c026d58
Riyad-07 <samimsamim1998@gmail.com>
2024-10-19 14:00:24 +0600

    db change

--- src/DB/FirebaseConfig.js
+++ src/DB/FirebaseConfig.js
@@ -1,4 +1,30 @@
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_kDXIeSfPaVPsY22bj9tvSRJrQAL-tvk",
+  authDomain: "chatapp-c9195.firebaseapp.com",
+  databaseURL: "https://chatapp-c9195-default-rtdb.firebaseio.com",
+  projectId: "chatapp-c9195",
+  storageBucket: "chatapp-c9195.appspot.com",
+  messagingSenderId: "257202542770",
+  appId: "1:257202542770:web:3f3ab19e99cdb9370dd1ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
 // import { initializeApp } from "firebase/app";
 // const firebaseConfig = {
     
@@ -20,17 +46,17 @@
 
 
 
-import { initializeApp } from "firebase/app";
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  databaseURL: import.meta.env.VITE_databaseURL,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId
-};
+// import { initializeApp } from "firebase/app";
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   databaseURL: import.meta.env.VITE_databaseURL,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId
+// };
 
 
-const app = initializeApp(firebaseConfig);
-export default firebaseConfig
+// const app = initializeApp(firebaseConfig);
+// export default firebaseConfig
