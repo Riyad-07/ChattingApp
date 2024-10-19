d97985b6801c9af1136ed5e3852225d6df65cbb2
Riyad-07 <samimsamim1998@gmail.com>
2024-10-19 13:16:56 +0600

    db update

--- src/DB/FirebaseConfig.js
+++ src/DB/FirebaseConfig.js
@@ -1,17 +1,17 @@
 
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
 const firebaseConfig = {
     
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
+  databaseURL:import.meta.env.VITE_databaseURL,
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
   appId: import.meta.env.VITE_appId
+
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app)
 
 export default firebaseConfig
